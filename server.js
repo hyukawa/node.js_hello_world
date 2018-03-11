082492b2ea8e30d8785e57bb833c6bb021dd33f1
hyukawa <hyukawa@live.jp>
2018-03-11 12:36:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -7,5 +7,3 @@ http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
     res.end('こんにちは');
 }).listen(PORT);
-
-console.log(`Server running at ${PORT}`);
