3e702fabf68b94de7ca9af414aca2dc14e0f2d57
hyukawa <hyukawa@live.jp>
2018-03-11 12:31:40 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-'use strict'
+'use strict';
 
-const HTTP = require('http');
-const PORT = process.env.PORT || 3001;
+const http = require('http');
+const PORT = 3000;
 
-HTTP.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node.js\n');
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
+    res.end('こんにちは');
 }).listen(PORT);
+
+console.log(`Server running at ${PORT}`);
